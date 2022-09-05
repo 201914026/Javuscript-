3ea362f64c95edeb59cbb9649cc21876ebda8b28
201914026 <102003072+201914026@users.noreply.github.com>
2022-09-05 11:34:43 +0900

    Add files via upload

---
+++ plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;
+    result=num1+num2;
+    return result
+}
+function miuns(num1,num2){
+    let result;
+    result=num1-num2;
+    return result
+}
+function omg(num1,num2){
+    let result;
+    result=num1*num2
+    return result
+}
+function lee(num1,num2){
+    let result;
+    result=unm1/unm2
+    return result
+}
