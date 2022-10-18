5f056074238835f9621eee2359e8b16bcfd6df65
201914026 <102003072+201914026@users.noreply.github.com>
2022-10-18 09:36:43 +0900

    Add files via upload

---
+++ 2학기/새 폴더/plus.js
@@ -0,0 +1,20 @@
+function plus(num1,num2){
+    let result;
+    result=num1+num2;
+    return result
+}
+function miuns(num1,num2){
+    let result;
+    result=num1-num2;
+    return result
+}
+function omg(num1,num2){
+    let result;
+    result=num1*num2
+    return result
+}
+function run(num1,num2){
+    let result;
+    result=num1/num2
+    return result
+}
